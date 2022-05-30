e3b72464fef0cbad77b8b4a2f99db7f5fa4468d5
I am <vasya_pupkin@pupkin.com>
2022-05-30 13:10:47 +0300

    Ups, i am write commit

--- task-11/var.tf
+++ task-11/var.tf
@@ -10,6 +10,21 @@ variable "git_token" {}
 variable "cl_crt" {}
 variable "cl_key" {}
 variable "cl_ca_key" {}
-variable "path_file" {}
+#variable "path_file" {}
 variable "host_ip" {}
+variable "git_repo_name" {}
+variable "path" {
+  default = "/home/dima/vm/terraform"
+}
+variable "files" {
+  default = [
+    "github.tf",
+    "provider.tf",
+    "static-deployment.tf",
+    "static-service.tf",
+    "terraform.tfvars",
+    "var.tf",
+    ".terraform.lock.hcl"
+  ]
+}
 
